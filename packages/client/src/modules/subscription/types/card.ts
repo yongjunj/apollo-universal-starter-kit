9ff660adfdddb0d34062efc97bb7eb1ab4540bcb
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-29 16:19:24 +0300

    Add base types for Subscription module

---
+++ packages/client/src/modules/subscription/types/card.ts
@@ -0,0 +1,37 @@
+/* --- ENTITIES --- */
+interface Card {
+  expiryMonth: number;
+  expiryYear: number;
+  last4: string;
+  brand: string;
+}
+
+interface CardUpdateOptions extends Card {
+  token: string;
+}
+
+interface CardInfo extends Card {
+  loading: boolean;
+}
+
+/* ---  TYPES --- */
+type UpdateCardFn = (options: CardUpdateOptions) => Promise<boolean>;
+
+/* --- COMPONENT PROPS --- */
+
+/**
+ * Mutation props
+ */
+interface CardOperation {
+  updateCard: UpdateCardFn;
+}
+
+/**
+ * Query props
+ */
+interface CardQueryResult {
+  subscriptionCardInfo: Card;
+}
+
+export { CardInfo };
+export { CardOperation, CardQueryResult };
